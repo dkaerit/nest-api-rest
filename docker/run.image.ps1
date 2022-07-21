2c33d437361c9380772ae911d7aafcc0a5487371
dkaerit <kaerit@hotmail.com>
2022-07-21 16:22:57 +0100

    startup

---
+++ docker/run.image.ps1
@@ -0,0 +1,8 @@
+docker build . -t $env:UserName/nest_backend
+
+# Container name: backtest
+# localhost: 8081
+# Container port: 8081
+# Volumen:
+#   Host path: (pwd)
+#   Container path: /usr/src/app
