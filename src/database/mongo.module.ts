6c5d4ba0635f8939dc1ce774c2661a6182d72ef8
Diego Vázquez Campos <kaerit@hotmail.com>
2023-12-12 20:36:56 +0000

    actu

--- src/database/mongo.module.ts
+++ src/database/mongo.module.ts
@@ -1,14 +1,6 @@
 import { MongooseModule } from '@nestjs/mongoose';
 import * as dotenv from 'dotenv'; dotenv.config();
 
-const tokenSecret = process.env.TOKEN_SECRET;
-const dbUsername = process.env.DB_USERNAME;
-const dbPassword = process.env.DB_PASSWORD;
-
-console.log('TOKEN_SECRET:', tokenSecret);
-console.log('DB_USERNAME:', dbUsername);
-console.log('DB_PASSWORD:', dbPassword);
-
 let username = process.env.DB_USERNAME,
     password = process.env.DB_PASSWORD,
     options = 'retryWrites=true&w=majority',
