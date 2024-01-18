d253a9c5ef23e9217fd844b4be7676a1139ff400
Diego Vázquez Campos <kaerit@hotmail.com>
2024-01-18 21:26:22 +0000

    post function

--- src/modules/post/post.schema.ts
+++ src/modules/post/post.schema.ts
@@ -7,7 +7,6 @@ export type PostDocument = Post & Document;
 @Schema()
 export class Post {
   @Prop({ required: true, index: true }) authorId: string;
-  @Prop({ required: true, index: true }) authorName: string;
   @Prop({ enum: ['user', 'character'], default: 'user', index: true }) authorType: string; // Nuevo campo para indicar si el autor es un usuario o un personaje
   @Prop({ required: true }) content: string;
   @Prop({ type: [{ type: String, ref: 'User' }] }) referencedUsers: string[]; // Lista de IDs de usuarios a los que hace referencia la publicación
